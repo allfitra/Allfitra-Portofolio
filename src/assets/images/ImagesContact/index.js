cd140b999c49bd5cf645c98273f6d960b3cabf2c
alfitrafadjri <alfitrafadjri00@gmail.com>
2025-05-05 15:58:30 +0700

    fix bug photo 2

--- src/assets/images/ImagesContact/index.js
+++ src/assets/images/ImagesContact/index.js
@@ -1,5 +1,5 @@
 import PhotoContact from './photo1.jpg';
-import PhotoContact2 from './photo22.jpg';
+import PhotoContact2 from './photo2.jpg';
 import PhotoContact3 from './photo3.jpg';
 
 export { PhotoContact, PhotoContact2, PhotoContact3 };
