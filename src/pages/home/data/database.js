8791901627a100c6810ddf4845df3ced8c25a0b6
alfitrafadjri <alfitrafadjri00@gmail.com>
2025-05-16 20:03:54 +0700

    update home description

--- src/pages/home/data/database.js
+++ src/pages/home/data/database.js
@@ -2,5 +2,5 @@ export const homeData = {
   name: 'Alfitra Fadjri',
   title: 'Software Engineer',
   description:
-    "I'm a tech enthusiast 🔥, with a solid background in web and application development. I'm a socially active person and have had experience in various bootcamps and internships.",
+    "I'm a tech enthusiast 🔥, with a solid background in web and application development. I'm a socially active person and have had experience in various bootcamps, internships and other experiences.",
 };
