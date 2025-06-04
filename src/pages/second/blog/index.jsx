32e9a996d2b87b4711c0f0aa7937f896f5772fcc
alfitrafadjri <alfitra.fadjri@digitalnayaka.com>
2025-06-04 14:29:06 +0700

    update format file and folder

---
+++ src/pages/second/blog/index.jsx
@@ -0,0 +1,12 @@
+import { SecondLayout } from '@/components/Layouts';
+import React from 'react';
+
+export const BlogPage = () => {
+  return (
+    <SecondLayout title="Blogs">
+      <div className="flex h-[80vh] w-full items-center justify-center">
+        <h1 className="text-3xl font-bold">Blogs Page is Coming Soon!</h1>
+      </div>
+    </SecondLayout>
+  );
+};
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,13 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 
-import { AnonymousPage } from '@/pages/anonymous-message';
-import { ContactPage } from '@/pages/contact';
-import { EducationPage } from '@/pages/education';
-import { ExperiencePage } from '@/pages/experience';
-import { HomePage } from '@/pages/home';
-import { MessagesListPage } from '@/pages/messages-list';
+import { AnonymousPage } from '@/pages/main/anonymous-message';
+import { ContactPage } from '@/pages/main/contact';
+import { EducationPage } from '@/pages/main/education';
+import { ExperiencePage } from '@/pages/main/experience';
+import { MessagesListPage } from '@/pages/main/messages-list';
 import { NotFoundPage } from '@/pages/not-found';
-import { ProjectPage } from '@/pages/project';
+import { ProjectPage } from '@/pages/main/project';
+import { BlogPage } from '@/pages/second/blog';
+import { HomePage } from '@/pages/main/home';
 
 export const AppRoutes = () => {
   return (
@@ -17,6 +18,8 @@ export const AppRoutes = () => {
       <Route path="/experience" element={<ExperiencePage />} />
       <Route path="/project" element={<ProjectPage />} />
       <Route path="/contact" element={<ContactPage />} />
+
+      <Route path="/blog" element={<BlogPage />} />
       <Route path="/anonymous-message" element={<AnonymousPage />} />
       <Route path="/230701" element={<MessagesListPage />} />
 
